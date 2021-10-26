d93511299278665a7e988a8c2dd11d6e3871a2df
Shaed Parkar <shaed.parkar@slimsolutions.company>
2021-10-26 10:59:22 +0100

    load form when component is loaded

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
 import { toast } from "react-toastify";
@@ -13,6 +13,13 @@ const ManageCoursePage = (props) => {
     authorId: null,
     category: "",
   });
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then((_course) => setCourse(_course));
+    }
+  }, [props.match.params.slug]);
 
   function handleChange({ target }) {
     setCourse({
